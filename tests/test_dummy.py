1e08a127dd9c07895af7fb9e8b4c45808a73c800
Cristhian Garcia <crisgarta8@gmail.com>
2024-02-20 12:35:41 -0500

    test: add dummy test for test suite

---
+++ tests/test_dummy.py
@@ -0,0 +1,11 @@
+"""
+Dummy test to check if the test suite is working.
+"""
+import pytest
+
+
+@pytest.mark.skip(reason="Placeholder to allow pytest to succeed before real tests are in place.")
+def test_placeholder():
+    """
+    TODO: Delete this test once there are real tests.
+    """
